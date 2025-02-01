cfe4d797b51d4af737baa123a0a3f358eeca28ee
shubhmpandey77 <cse.19bcs4577@gmail.com>
2025-02-02 00:21:56 +0530

    continue to setup git security

--- backend/src/main/java/com/job_tracker_webapp/JobTrackerWebappApplication.java
+++ backend/src/main/java/com/job_tracker_webapp/JobTrackerWebappApplication.java
@@ -1,10 +1,13 @@
 package com.job_tracker_webapp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class JobTrackerWebappApplication {
+
+	private static Dotenv dotenv = Dotenv.load();
+	private static String dbUrl = dotenv.get("DATABASE_URL");
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobTrackerWebappApplication.class, args);
