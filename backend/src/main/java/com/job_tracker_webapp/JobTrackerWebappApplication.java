5c99bc4ea1dcda60a55cb5ba9fab684d866012b0
shubhmpandey77 <cse.19bcs4577@gmail.com>
2025-02-02 00:05:07 +0530

    Initial project setup

---
+++ backend/src/main/java/com/job_tracker_webapp/JobTrackerWebappApplication.java
@@ -0,0 +1,13 @@
+package com.job_tracker_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackerWebappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/job_tracker_webapp/JobTrackerWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job_tracker_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
